d4275a74410c661dd5981e3cfb5135e79bcc028e
MaslennikovSergey <nevadapost@gmail.com>
2023-01-04 17:12:30 +0300

    init sqlite3

--- service/admin.py
+++ service/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Bin
+from .models import Product, Bin, Home
 
 # Register your models here.
 
@@ -10,9 +10,17 @@ admin.site.register(Bin)
 
 class ProductAdmin(admin.ModelAdmin):
     
-    list_display = ['id', 'cost']
+    list_display = ['id', 'cost', 'is_active']
     fields = ['id']
     list_filter = ['cost']
     search_fields = ['']
 
 #admin.site.register(Product, admin.site.register(Product))
+
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    
+    list_display = ['size', 'cost', 'adr', 'bal']
+    #fields = ['id']
+    #list_filter = ['cost']
+    #search_fields = ['']
